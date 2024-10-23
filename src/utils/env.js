1ab4c59e7b9a6f4f6b70c26f13b6773fd5c10c17
Victoria <vika96karnauhova@gmail.com>
2024-10-24 00:46:48 +0300

    add tasks

--- src/utils/env.js
+++ src/utils/env.js
@@ -11,9 +11,3 @@ export function env(name, defaultValue) {
 
   throw new Error(`Missing: process.env['${name}'].`);
 }
-
-console.log('MONGODB_USER:', process.env.MONGODB_USER);
-console.log('MONGODB_PASSWORD:', process.env.MONGODB_PASSWORD);
-console.log('MONGODB_URL:', process.env.MONGODB_URL);
-console.log('MONGODB_DB:', process.env.MONGODB_DB);
-console.log('PORT:', process.env.PORT);
